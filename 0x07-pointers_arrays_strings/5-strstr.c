d0d2eef31991b7724b0adb7754d5632eb8337c53
Edmond MUKUNDE <edmondmukunde4@gmail.com>
2023-04-03 16:39:28 -0700

    pointers, array and strings

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,23 +11,21 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; haystack[i] != '\0'; i++)
-    {
-        for (j = 0; needle[j] != '\0'; j++)
-        {
-            if (haystack[i + j] != needle[j])
-            {
-                break;
-            }
-        }
-
-        if (needle[j] == '\0')
-        {
-            return (haystack + i);
-        }
-    }
-
-    return (NULL);
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+			{
+				break;
+			}
+		}
+		if (needle[j] == '\0')
+		{
+			return (haystack + i);
+		}
+	}
+	return (NULL);
 }
