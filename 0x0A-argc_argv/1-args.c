9f21ac12cbafbb44ec994efc959bb73f56cc2900
Edmond MUKUNDE <edmondmukunde4@gmail.com>
2023-04-10 06:23:49 -0700

    argc and argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - function that counts arguments passed except the first.
+ * @argc: argument count.
+ * @argv: argument value.
+ * Return: 0 (success)
+ */
+
 int main(int argc, char __attribute__((unused))*argv[])
 {
 	printf ("%d\n", argc - 1);
