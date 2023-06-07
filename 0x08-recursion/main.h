f10cfb1c8aa3b65e092088d0e1bec3f23c67f53a
Edmond MUKUNDE <edmondmukunde4@gmail.com>
2023-06-07 14:40:24 -0700

    a function that calculates the length of the string.

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,9 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 void _puts_recursion(char *s);
 int _putchar(char c);
 void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
 
 #endif /*MAIN_H*/
